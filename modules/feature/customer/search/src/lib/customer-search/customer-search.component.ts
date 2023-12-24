e69d92b2500e797d4cba0784269177c9f6398636
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-12-24 20:16:55 -0300

    feat(customer-search): add lib

---
+++ modules/feature/customer/search/src/lib/customer-search/customer-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'customer-registration-customer-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customer-search.component.html',
+  styleUrl: './customer-search.component.scss',
+})
+export class CustomerSearchComponent {}
