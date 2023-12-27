743d4c2bd34ee6dbe09f551a8d7c86e394204044
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-12-27 17:05:58 -0300

    feat(customer-registration): add http errors interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { LayoutModule } from '@customer-registration/layout';
 import { CustomerSearchComponent } from '@customer-registration/customer-search';
+import { HttpErrorsInterceptor } from './interceptors/http-errors/http-errors.interceptor';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,8 +19,15 @@ import { CustomerSearchComponent } from '@customer-registration/customer-search'
     LayoutModule,
     BrowserAnimationsModule,
     CustomerSearchComponent,
+    NgbAlertModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/interceptors/http-errors/http-errors.interceptor.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import Swal from 'sweetalert2';
+
+@Injectable()
+export class HttpErrorsInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const clonedRequest = request.clone({
+      setHeaders: {
+        'x-access-token': 'MEU_TOKEN',
+      },
+    });
+
+    return next.handle(clonedRequest).pipe(
+      catchError((error) => {
+        Swal.fire({
+          icon: 'error',
+          text: 'houve um erro',
+          toast: true,
+          position: 'bottom',
+          timer: 5000,
+          showConfirmButton: false,
+        });
+        return throwError(() => error);
+      })
+    );
+  }
+}
