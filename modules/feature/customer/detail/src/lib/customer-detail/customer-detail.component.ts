8ad4dcc48d548469965947222d387e92beb32930
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-12-28 11:49:22 -0300

    feat(customer-detail): add edit and delete options

--- modules/feature/customer/detail/src/lib/customer-detail/customer-detail.component.ts
+++ modules/feature/customer/detail/src/lib/customer-detail/customer-detail.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable, map, switchMap, tap } from 'rxjs';
 import { CustomerCardComponent } from '@customer-registration/customer-ui';
 import { CpfFormatPipe } from '@customer-registration/customer-data-access';
 import {
   Customer,
   CustomerSearchService,
 } from '@customer-registration/customer-data-access';
+import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from '@customer-registration/register';
 
 function getParamId(): Observable<string> {
   return inject(ActivatedRoute).params.pipe(map((params) => params['id']));
@@ -24,5 +27,36 @@ export class CustomerDetailComponent {
   public customer$: Observable<Customer> = getParamId().pipe(
     switchMap((id) => this.customerSearchService.getById(id))
   );
-  constructor(private customerSearchService: CustomerSearchService) {}
+  constructor(
+    private customerSearchService: CustomerSearchService,
+    private modalService: NgbModal
+  ) {}
+
+  onDelete(id: string) {
+    Swal.fire({
+      title: 'Excluir cliente',
+      text: 'Tem certeza que deseja excluir o cliente?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim, excluir',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.customer$ = this.customer$.pipe(
+          tap(this.customerSearchService.deleteById(id))
+        );
+      }
+    });
+  }
+
+  onEdit(id: string, customer: Customer) {
+    const modal = this.modalService.open(RegisterComponent, {
+      size: 'lg',
+    });
+    modal.componentInstance.customer = customer;
+    modal.result.then((result) => {
+      this.customer$ = this.customerSearchService.updateById(id, result);
+    });
+  }
 }
